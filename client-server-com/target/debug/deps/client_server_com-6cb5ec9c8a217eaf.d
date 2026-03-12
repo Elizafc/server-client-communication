4e2db474ac84497cde14905a538e5c66c9605b23
Eliza Freitas <elizafc20@gmail.com>
2026-03-12 20:49:46 -0300

    criação do repo cargo para fazer a atividade

---
+++ client-server-com/target/debug/deps/client_server_com-6cb5ec9c8a217eaf.d
@@ -0,0 +1,5 @@
+/home/elizafc/server-client-communication/client-server-com/target/debug/deps/client_server_com-6cb5ec9c8a217eaf.d: src/main.rs
+
+/home/elizafc/server-client-communication/client-server-com/target/debug/deps/libclient_server_com-6cb5ec9c8a217eaf.rmeta: src/main.rs
+
+src/main.rs:
